2a34b00ff1ec697ba1d138258a5e9277fb4b245d
GianmarioC <cerciellogianmario59@gmail.com>
2023-10-21 12:05:42 +0200

    primoCommit

---
+++ src/main/java/com/prova/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.prova.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/ciao")
+    public String ciao(){
+        return "Ciao!";
+    }
+    
+}
